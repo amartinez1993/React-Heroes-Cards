19759a8e413f107aa3632d2416b2ddcabc1a9c7f
Adrià Martinez Moreno <ammoreno@gigames.es>
2023-09-20 17:33:20 +0200

    learn route protection

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,10 +1,11 @@
+import { AuthProvider } from "./auth";
 import { AppRouter } from "./router/AppRouter"
 import 'animate.css';
 
 export const HeroesApp = () => {
   return (
-    <>
+    <AuthProvider>
         <AppRouter />
-    </>
+    </AuthProvider>
   )
 }
